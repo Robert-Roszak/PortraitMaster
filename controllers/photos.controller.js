247cc955033eb1a2d7bd0709b74e3b3190f5dc62
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-11-29 20:55:53 +0100

    Add server side data validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const sanitize = require('mongo-sanitize');
 
 /****** SUBMIT PHOTO ********/
 
@@ -6,15 +7,30 @@ exports.add = async (req, res) => {
 
   try {
     const { title, author, email } = req.fields;
+
+    for (const param in req.fields) {
+      sanitize(req.fields[param]);
+    };
+
+    function escape(html) {
+      return html.replace(/&/g, "&amp;")
+                 .replace(/</g, "&lt;")
+                 .replace(/>/g, "&gt;")
+                 .replace(/"/g, "&quot;")
+                 .replace(/'/g, "&#039;");
+    };
+
+    const cleanTitle = escape(title);
+    const cleanAuthor = escape(author);
+
     const file = req.files.file;
+    const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+    const fileExt = fileName.split('.').slice(-1)[0];
 
-    if(title && author && email && file) { // if fields are not empty...
-
-      const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
+    if(cleanTitle.length <= 25 && cleanAuthor.length <= 50 && email.includes('@') && file && (fileExt === 'jpg' || fileExt === 'gif' || fileExt === 'png')) {
+      const newPhoto = new Photo({ title: cleanTitle, author: cleanAuthor, email, src: fileName, votes: 0 });
+      await newPhoto.save();
       res.json(newPhoto);
-
     } else {
       throw new Error('Wrong input!');
     }
